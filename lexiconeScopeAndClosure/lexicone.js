cc0d4547cba5c6cd4d27596850ed0dfa299bf0fa
AkumuJey <josephowino7@gmail.com>
2023-01-21 11:46:32 +0300

    lexicone scope and closure

---
+++ lexiconeScopeAndClosure/lexicone.js
@@ -0,0 +1,18 @@
+let x = 1;
+
+const parentFunction = () => {
+    let myValue = 2;
+    console.log(x);
+    console.log(myValue);
+    const childFunction = () => {
+        console.log(x += 5);
+        console.log(myValue -= 5);
+    }
+    childFunction()
+}
+
+parentFunction()
+
+/**
+ * Lexicone scope and not closure. The child function has access to parent function and global variables.
+ */
