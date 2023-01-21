1f7563cb0782313321ed35e18bbb9787c6a41299
AkumuJey <josephowino7@gmail.com>
2023-01-21 12:31:13 +0300

    Closure explained with IIFE

--- lexiconeScopeAndClosure/closureadvanced.js
+++ lexiconeScopeAndClosure/closureadvanced.js
@@ -9,3 +9,32 @@ const privateCounter = (() => {
     console.log(count);}
 })();
 
+privateCounter()
+privateCounter()
+privateCounter()
+privateCounter()
+
+const credits = ((num) => {
+    let credits = num
+    console.log(`Initial credits value: ${credits}`);
+    return () => {
+        credits = (credits * Math.random() * 8.8).toFixed(0)
+        if (credits % 2 == 0) {
+            console.log(`${credits} is even`);
+        } else console.log(`${credits} is odd`);
+    }
+})(3)
+
+
+credits()
+credits()
+credits()
+credits()
+credits()
+credits()
+credits()
+credits()
+credits()
+credits()
+credits()
+credits()
