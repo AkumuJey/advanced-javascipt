1073a22f6621a06589d4200974747557aa862ad6
AkumuJey <josephowino7@gmail.com>
2023-01-22 23:01:03 +0300

    Inheritance and prototype introduction

---
+++ prototypes/protypeInheritanceChain.js
@@ -0,0 +1,16 @@
+const person = {
+    alive : true
+}
+
+const musician = {
+    plays: true
+}
+
+console.log(musician.plays); /**True */
+console.log(musician.alive); /**Undefined */
+
+
+musician.__proto__ = person /**Old method of doing things */
+
+console.log(musician.alive); /**True inherited alive attribute */
+console.log(musician);
