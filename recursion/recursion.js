be6f07ac1fa987cba9c386d218670baddbfcbc4f
AkumuJey <josephowino7@gmail.com>
2023-01-26 15:57:21 +0300

    introduction to recursion concept

---
+++ recursion/recursion.js
@@ -0,0 +1,27 @@
+const countToTen = (num = 1) => {
+    while (num <= 10) {
+        console.log(num);
+        num += 1
+    }
+}
+
+// countToTen()
+
+/**Recursive functions have 2 parts 
+ * 1) Recursive call to the function.
+ * 2) At least one condition to exit.
+ */
+
+
+const recurTen = (num = 21) => {
+   if (num > 30) return;
+   console.log(num);
+   num ++
+   recurTen(num)
+}
+
+recurTen()
+
+/**Recurssion creates readerbility and keeps code dry.
+ * Affects permormance and is hard to debug
+ */
