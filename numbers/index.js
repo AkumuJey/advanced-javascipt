79706ab8532f0f88dbeb7bc7535636d7c52ee363
AkumuJey <josephowino7@gmail.com>
2023-04-01 12:59:53 +0300

    JS Numbers done

---
+++ numbers/index.js
@@ -0,0 +1,20 @@
+//scientific notation can also be sued
+let x = 123e5; //123 * 10**5 = 12300000
+let y = 123e-5; //123 * 10 ** -5 =0.00123
+
+//interger precision is upto 15 diits
+let p = 999999999999999;   // x will be 999999999999999
+let q = 9999999999999999;  // y will be 10000000000000000
+
+
+//Bigint used to store intergers biger than 1 5 digits
+let num1 = 1234567890123456789012345n;
+let num2 = BigInt(1234567890123456789012345)
+
+//Bigint cannot accept decimals
+
+
+//Global javascript methods
+// Number()	Returns a number converted from its argument.
+// parseFloat()	Parses its argument and returns a floating point number
+// parseInt()	Parses its argument and returns a whole number
