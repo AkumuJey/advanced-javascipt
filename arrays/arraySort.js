fc3feb3ea92c9a31d5a9e1266c19b19ed1b04a97
AkumuJey <josephowino7@gmail.com>
2023-04-04 16:41:05 +0300

    Array methods done

--- arrays/arraySort.js
+++ arrays/arraySort.js
@@ -16,3 +16,42 @@ console.log(points.sort(() => 0.5 - Math.random()));
 
 
 //Fisher Yate Shuffle
+const yateShuffle = (array) => {
+    for (let i = array.length -1; i > 0 ; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+        let k = array[i]
+        array[i] = array[j]
+        array[j] = k
+    }
+    console.log(array);
+}
+
+console.log(scores);
+yateShuffle(scores)
+console.log(scores);
+
+let ages = [12, 45, 66, 79, 32, 44, 27, 89, 93]
+//First method of determining the maximum and minimum
+    let sortedAges = ages.sort((a, b) => a -b)
+    console.log(sortedAges[0]); //Smalletst
+    console.log(sortedAges[sortedAges.length - 1]); //Largest
+
+//Maximum age using max method
+let findMaxOrMin = (array, num) => {
+    if (num > 0) {
+        return Math.max.apply(null, array)
+    }
+    return Math.min.apply(null, array)
+}
+
+console.log(findMaxOrMin(ages, 1));
+console.log(findMaxOrMin(ages, 0));
+
+const cars = [
+    {type:"Volvo", year:2016},
+    {type:"Saab", year:2001},
+    {type:"BMW", year:2010}
+  ];
+
+//sorting method
+cars.sort((a, b) => a.year - b.year)
