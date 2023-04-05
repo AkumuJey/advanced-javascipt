28cb2300ab31c532bad54e50656b84720c4b474c
AkumuJey <josephowino7@gmail.com>
2023-04-05 22:37:08 +0300

    Intriduction to objects

---
+++ objects/index.js
@@ -0,0 +1,14 @@
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue",
+    fullName: function () {
+        return this.firstName + " " + this.lastName
+    }
+  };
+
+  console.log(person.firstName);
+  console.log(person["lastName"]);
+  console.log(person.fullName());
+
