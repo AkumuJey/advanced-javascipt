cf615bb5b05b9dd0eb0210065a6df92334660308
AkumuJey <josephowino7@gmail.com>
2023-04-05 23:10:45 +0300

    Objects are conclusive

--- objects/index.js
+++ objects/index.js
@@ -5,10 +5,40 @@ const person = {
     eyeColor: "blue",
     fullName: function () {
         return this.firstName + " " + this.lastName
-    }
+    },
   };
 
   console.log(person.firstName);
   console.log(person["lastName"]);
   console.log(person.fullName());
 
+
+  //Getters and Setters
+  const personX = {
+    firstName: "John",
+    lastName: "Doe",
+    language: "en",
+    get lang() {
+      return this.language;
+    }
+  };
+
+  const personY = {
+    firstName: "John",
+    lastName: "Doe",
+    language: "",
+    set lang(lang) {
+      this.language = lang;
+    }
+  };
+
+  console.log(personX.lang);
+
+  function Student (name, age, regNo) {
+    this.name = name,
+    this.age = age,
+    this.regNo = regNo
+  }
+
+  const yara = new Student("Yara", 25, 1915)
+  console.log(yara.age);
