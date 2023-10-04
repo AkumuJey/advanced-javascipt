5fb2483374ede319f9daaabaf2ced4275b87040c
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-10-04 21:48:57 +0300

    Orientation with dom manipulation basics

--- html-css-and-js-review/index.js
+++ html-css-and-js-review/index.js
@@ -5,3 +5,21 @@ let myButton = document.querySelector('button')
 myButton.addEventListener('click', () => {
     document.getElementById('content').textContent = "I am working"
 })
+
+const imageChanger = document.getElementById('imageChanger')
+imageChanger.addEventListener('click', () => {
+    let imageOne = '../images/firefoxBlue.jpeg'
+    let imageTwo = '../images/firefoxYellow.jpeg'
+    let myImage = document.querySelector('img')
+    let mySrc = myImage.getAttribute('src')
+    if (mySrc === imageOne) {
+        myImage.setAttribute('src', imageTwo)
+    } else{
+        myImage.setAttribute('src', imageOne)
+    }
+})
+window.addEventListener('load', () => {
+    const userInput = prompt('Who are you')
+    let x = confirm(`Are you ready ${userInput}`)
+    alert(`Bingo! ${x}`)
+})
