3e6ec9df870ed0d54c6262b51d479751236d9577
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-10-14 21:10:01 +0300

    aync await functions

--- objects/promise.js
+++ objects/promise.js
@@ -10,4 +10,34 @@ console.log(fetchPromise);
 // })
 
 //Refactored format
-fetchPromise.then(response => response.json()).then((data) => console.log(data[0].name))
+fetchPromise.then(response => {
+    if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`)
+    }
+    return response.json();
+}).then((data) => console.log(data[0].name)).catch((error) => console.log(error))
+
+
+// Combining promises using Promise.all([])
+const fetchPromise1 = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",
+  );
+  const fetchPromise2 = fetch(
+    "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/not-found",
+  );
+  const fetchPromise3 = fetch(
+    "https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json",
+  );
+  
+  Promise.all([fetchPromise1, fetchPromise2, fetchPromise3])
+    .then((responses) => {
+      for (const response of responses) {
+        console.log(`${response.url}: ${response.status}`);
+      }
+    })
+    .catch((error) => {
+      console.error(`Failed to fetch: ${error}`);
+    });
+
+// Promise.any()
+  
