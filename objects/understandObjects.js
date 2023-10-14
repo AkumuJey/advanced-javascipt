2df7f458d5e6343f0af23668cd39dc9c66e91f9c
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-10-14 13:05:43 +0300

    Nested objects

---
+++ objects/understandObjects.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Akumu',
+    age: 45
+}
+
+const countriesVisited = ['Kenya', {
+    visited: 'yes',
+    favorite: 'true'
+}, 'Tanzania']
+
+person.countriesVisited = countriesVisited
+
+console.log(person);
