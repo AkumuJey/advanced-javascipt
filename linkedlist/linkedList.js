a7613ac6ba57fe4da7d7247c27181ff706057ae5
Akumu Joseph Owino <109744817+AkumuJey@users.noreply.github.com>
2023-10-23 22:14:26 +0300

    Inserting fists and last in a node

--- linkedlist/linkedList.js
+++ linkedlist/linkedList.js
@@ -1,11 +1,61 @@
-const n1 = {
-    data: 200
+// const y1 = {
+//     data: 200
+// }
+
+// const y2 = {
+//     data: 300
+// }
+
+// y1.next = y2
+
+// console.log(y1);
+
+class Node {
+    constructor (data, next=null) {
+        this.data = data,
+        this.next = next
+    }
 }
 
-const n2 = {
-    data: 300
+class LinkedList {
+    constructor(){
+        this.head = null
+        this.size = 0
+    }
+    //inserting first node
+    insertFirstNode(data){
+        this.head = new Node(data, this.head)
+        this.size ++
+    }
+    //insert last
+    insertLastNode(data){
+        let node = new Node(data)
+        let current;
+        if(!this.head) {
+            this.head = node
+        } else {
+            current = this.head
+            while(current.next) {
+                current = current.next
+            }
+            current.next = node
+        }
+        this.size ++
+    }
+    //print linked list
+    printListData(){
+        let current = this.head
+        while(current !== null){
+            console.log(current.data)
+            current = current.next
+        }
+    }
 }
 
-n1.next = n2
+const ll = new LinkedList()
+ll.insertFirstNode(100)
+ll.insertFirstNode(300)
+ll.insertLastNode(1000)
+// console.log(ll)
 
-console.log(n1);
+ll.printListData()
---
+++ linkedlist/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+const y1 = {
+//     data: 200
+// }
+
+// const y2 = {
+//     data: 300
+// }
+
+// y1.next = y2
+
+// console.log(y1);
